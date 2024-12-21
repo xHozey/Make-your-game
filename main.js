b051e393995d613da22aface909439d4ec306c2f
Hamza <hamzasaliheddine@hotmail.com>
2024-12-21 10:43:27 +0100

    random soft-wall spawn

--- main.js
+++ main.js
@@ -1,80 +1,48 @@
 import { level_1 } from "./levels.js";
-let map = document.querySelector(".map");
+const map = document.querySelector(".map");
 const grids = [];
+let portal = false;
+for (let i = 0; i < 120; i++) {
+  const row = Math.floor(Math.random() * level_1.length);
+  const col = Math.floor(Math.random() * level_1[0].length);
+
+  if (!portal && level_1[row][col] == 0) {
+    level_1[row][col] = 3;
+    portal = true;
+    continue;
+  }
+  if (level_1[row][col] == 0) level_1[row][col] = 2;
+}
 
 for (let i = 0; i < level_1.length; i++) {
   grids[i] = [];
   for (let j = 0; j < level_1[i].length; j++) {
-    let div = document.createElement("div");
-    grids[i].push(div);
+    const div = document.createElement("div");
+    grids.push(div);
     map.appendChild(div);
-    switch (level_1[i][j]) {
-      case 0:
-        div.classList.add("empty");
-        break;
-      case 1:
-        div.classList.add("wall");
-      default:
-        break;
+    if (level_1[i][j] == 0 || level_1[i][j] == "x") {
+      div.classList.add("empty");
+      continue;
+    }
+    if (level_1[i][j] == 1) {
+      div.classList.add("wall");
+      continue;
+    }
+    if (level_1[i][j] == 2) {
+      div.classList.add("soft-wall");
+      continue;
+    }
+    if (level_1[i][j] == 3) {
+      div.classList.add("soft-wall", "portal");
+      continue;
     }
   }
 }
 
-let bomberman = document.createElement("div");
-bomberman.classList.add("bomber-man");
-const playerPos = { x: 30, y: 30 };
-let targetPos = { ...playerPos };
+const win = () => {
+  //player position in the portal and all enemies are dead
+}
 
-bomberman.style.transform = `translate(${playerPos.x}px, ${playerPos.y}px)`;
-map.appendChild(bomberman);
-
-const movePlayer = (e) => {
-  let key = e.key.toLowerCase();
-  const playerSpeed = 10;
-
-  switch (key) {
-    case "arrowup":
-      if (
-        targetPos.y / 30 > 0 &&
-        level_1[targetPos.y / 30 - 1][targetPos.x / 30] !== 1
-      ) {
-        targetPos.y -= playerSpeed;
-      }
-      break;
-    case "arrowdown":
-      if (
-        targetPos.y / 30 < level_1.length - 1 &&
-        level_1[targetPos.y / 30 + 1][targetPos.x / 30] !== 1
-      ) {
-        targetPos.y += playerSpeed;
-      }
-      break;
-    case "arrowleft":
-      if (
-        targetPos.x / 30 > 0 &&
-        level_1[targetPos.y / 30][targetPos.x / 30 - 1] !== 1
-      ) {
-        targetPos.x -= playerSpeed;
-      }
-      break;
-    case "arrowright":
-      if (
-        targetPos.x / 30 < level_1[0].length - 1 &&
-        level_1[targetPos.y / 30][targetPos.x / 30 + 1] !== 1
-      ) {
-        targetPos.x += playerSpeed;
-      }
-      break;
-    default:
-      break;
-  }
-
-  requestAnimationFrame(animateMovement);
-};
-
-const animateMovement = () => {
-  bomberman.style.transform = `translate(${targetPos.x}px, ${targetPos.y}px)`;
-  requestAnimationFrame(animateMovement);
-};
-
-document.addEventListener("keydown", movePlayer);
+const lose = () => { 
+  //player get expolied by bomber or hited by enemy
+}
