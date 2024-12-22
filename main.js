f2dd18b5cf92711d86c02f0656f1d54ab3a2e09e
Hamza <hamzasaliheddine@hotmail.com>
2024-12-22 18:34:12 +0100

    trying to merge

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import { level_1 } from "./levels.js";
 const map = document.querySelector(".map");
+console.log(level_1.length, level_1[0].length);
+
 const grids = [];
 let portal = false;
-for (let i = 0; i < 120; i++) {
+for (let i = 0; i < 450; i++) {
   const row = Math.floor(Math.random() * level_1.length);
   const col = Math.floor(Math.random() * level_1[0].length);
 
@@ -18,7 +20,7 @@ for (let i = 0; i < level_1.length; i++) {
   grids[i] = [];
   for (let j = 0; j < level_1[i].length; j++) {
     const div = document.createElement("div");
-    grids.push(div);
+    grids[i].push(div);
     map.appendChild(div);
     if (level_1[i][j] == 0 || level_1[i][j] == "x") {
       div.classList.add("empty");
@@ -49,28 +51,63 @@ const lose = () => {
 
 let bomberman = document.createElement("div");
 bomberman.classList.add("bomber-man");
-const playerPos = { x: 30, y: 30 };
-let targetPos = { ...playerPos };
+const playerPos = { x: 32, y: 32 };
+let currentPos = { ...playerPos };
 
 bomberman.style.transform = `translate(${playerPos.x}px, ${playerPos.y}px)`;
 map.appendChild(bomberman);
 
 const movePlayer = (e) => {
   let key = e.key.toLowerCase();
-  const playerSpeed = 10;
-
+  const playerSpeed = 16;
   switch (key) {
     case "arrowup":
-      targetPos.y -= playerSpeed;
+      if (
+        grids[Math.floor(playerPos.y / 16 - 1)][Math.floor(playerPos.x / 16)].classList.contains(
+          "wall"
+        ) ||
+        grids[Math.floor(playerPos.y / 16 - 1)][Math.floor(playerPos.x / 16)].classList.contains(
+          "soft-wall"
+        )
+      )
+        return;
+      playerPos.y -= playerSpeed;
       break;
     case "arrowdown":
-      targetPos.y += playerSpeed;
+      if (
+        grids[Math.floor(playerPos.y / 16 + 1)][Math.floor(playerPos.x / 16)].classList.contains(
+          "wall"
+        ) ||
+        grids[Math.floor(playerPos.y / 16 + 1)][Math.floor(playerPos.x / 16)].classList.contains(
+          "soft-wall"
+        )
+      )
+        return;
+      playerPos.y += playerSpeed;
       break;
     case "arrowleft":
-      targetPos.x -= playerSpeed;
+      if (
+        grids[Math.floor(playerPos.y / 16)][Math.floor(playerPos.x / 16 - 1)].classList.contains(
+          "wall"
+        ) ||
+        grids[Math.floor(playerPos.y / 16)][Math.floor(playerPos.x / 16 - 1)].classList.contains(
+          "soft-wall"
+        )
+      )
+        return;
+      playerPos.x -= playerSpeed;
       break;
     case "arrowright":
-      targetPos.x += playerSpeed;
+      if (
+        grids[Math.floor(playerPos.y / 16)][Math.floor(playerPos.x / 16 + 1)].classList.contains(
+          "wall"
+        ) ||
+        grids[Math.floor(playerPos.y / 16)][Math.floor(playerPos.x / 16 + 1)].classList.contains(
+          "soft-wall"
+        )
+      )
+        return;
+      playerPos.x += playerSpeed;
       break;
     default:
       break;
@@ -80,8 +117,11 @@ const movePlayer = (e) => {
 };
 
 const animateMovement = () => {
-  bomberman.style.transform = `translate(${targetPos.x}px, ${targetPos.y}px)`;
-  requestAnimationFrame(animateMovement);
+  bomberman.style.transform = `translate(${playerPos.x}px, ${playerPos.y}px)`;
+  if (playerPos.x != currentPos.x || playerPos.y != currentPos.y) {
+    currentPos = { ...playerPos };
+    requestAnimationFrame(animateMovement);
+  }
 };
 
 document.addEventListener("keydown", movePlayer);
